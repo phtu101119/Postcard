690859fa61327ee8caa30548bbb22d8e9c7e7590
Tuan Pham-Minh <Toni@Tuans-MacBook-Pro.local>
2016-02-24 20:41:18 +0100

    added Name-Label

--- testswift/ViewController.swift
+++ testswift/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,13 +29,21 @@ class ViewController: UIViewController {
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         messageLabel.hidden = false
-        messageLabel.text = "Hello " + enterMessageTextField.text!
+        messageLabel.text = enterMessageTextField.text!
         messageLabel.textColor = UIColor.blueColor()
         
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.redColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
         mailButton.setTitle("Mail sent", forState: UIControlState.Normal)
+        
     }
 
 }
